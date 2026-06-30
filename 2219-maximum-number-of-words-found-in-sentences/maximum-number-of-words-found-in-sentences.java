d5a7b946ff4ee2b5c5b9ea8bd4c3ca48ff6d595a
Shravya Sree Kotagiri <kotagirishravyasree@gmail.com>
2026-06-30 10:59:31 +0530

    Time: 4 ms (41.76%) | Memory: 44 MB (89.37%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -1,6 +1,7 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        int[] arr=new int [sentences.length];
+        int max=0;
+    //    int[] arr=new int [sentences.length];
         for(int i=0;i<sentences.length;i++)
         {
             int words=1;
@@ -12,18 +13,22 @@ class Solution {
                     words++;
                 }
             }
-            arr[i]=words;
+    //        arr[i]=words;
+            if(words>max)
+            {
+                max=words;
+            }
 
             
         }
-        int max=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]>max)
-            {
-                max=arr[i];
-            }
-        }
+        // int max=0;
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     if(arr[i]>max)
+        //     {
+        //         max=arr[i];
+        //     }
+        // }
         return max;
         
     }
