c325b370155ac1e78a1688990c0afa124932bdd1
Shravya Sree Kotagiri <kotagirishravyasree@gmail.com>
2026-07-17 22:34:58 +0530

    Time: 9 ms (29.35%) | Memory: 47.3 MB (5.11%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] arr1=s.toCharArray();
+        char[] arr2=t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        String s1=Arrays.toString(arr1);
+        String s2=Arrays.toString(arr2);
+        if(s1.equals(s2)) return true;
+        return false;
+        // for(int i=0;i<arr1.length;i++)
+        // {
+        //     if(arr1[i]!=arr2[i])
+        //     {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+    }
+}
