dd1a3f1344b4fdf63ddbb7032502454f5f08cc62
Shravya Sree Kotagiri <kotagirishravyasree@gmail.com>
2026-07-26 13:49:16 +0530

    Time: 7 ms (45.81%) | Memory: 55.5 MB (59.92%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,21 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer,Integer> hm=new HashMap<>();
-        int majority=-1;
-        int half=nums.length/2;
-        for(int i=0;i<nums.length;i++)
-        {
-            int n=nums[i];
-            hm.put(n,hm.getOrDefault(n,0)+1);
-        }
-        for(int ele:hm.keySet())
-        {
-            if(hm.get(ele)>half)
-            {
-               return ele;
-            }
-        }
-        return majority;
+        // HashMap <Integer,Integer> hm=new HashMap<>();
+        // int majority=-1;
+        // int half=nums.length/2;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     int n=nums[i];
+        //     hm.put(n,hm.getOrDefault(n,0)+1);
+        // }
+        // for(int ele:hm.keySet())
+        // {
+        //     if(hm.get(ele)>half)
+        //     {
+        //        return ele;
+        //     }
+        // }
+        // return majority;
+
+
+        Arrays.sort(nums);
+        return nums[nums.length/2];
         
     }
 }
