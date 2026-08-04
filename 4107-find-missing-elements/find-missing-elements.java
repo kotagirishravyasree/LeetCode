ccd147d56670481492a13ce9e54d77f6a4cfcc34
Shravya Sree Kotagiri <kotagirishravyasree@gmail.com>
2026-08-04 20:51:44 +0530

    Time: 7 ms (22.50%) | Memory: 47.1 MB (25.78%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -14,6 +14,7 @@ class Solution {
                 if(nums[j]==ele)
                 {
                     present=true;
+                    break;
                 }
             }
             if(present==false)
