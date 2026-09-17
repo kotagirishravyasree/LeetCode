6fe0e60195267d92c0667c81c479df2673258256
Shravya Sree Kotagiri <kotagirishravyasree@gmail.com>
2026-09-17 21:38:15 +0530

    Time: 3 ms (27.86%) | Memory: 43.6 MB (24.20%) - LeetSync

---
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int secondHighest(String s) {
+        int max = -1;
+        int secMax = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                int num = ch - '0';
+
+                if (num > max) {
+                    secMax = max;
+                    max = num;
+                }
+                else if (num > secMax && num != max) {
+                    secMax = num;
+                }
+            }
+        }
+
+        return secMax;
+    }
+}
